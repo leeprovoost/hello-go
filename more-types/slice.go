0a8789d6cfa82f60062b63fea61a61a3012b206f
Lee Provoost <lee.provoost@gmail.com>
2015-07-18 13:31:54 +0200

    added append and range for slices

--- more-types/slice.go
+++ more-types/slice.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	a := []string{"Lee", "Provoost"}
 	fmt.Println(a[0], a[1])
+
+	b := make([]int, 5)
+	fmt.Println(b)
+	b = append(b, 1)
+	fmt.Println(b)
+	b[0] = 3
+	b[1] = 10
+	b[2] = 4
+	fmt.Println(b)
+
+	for i, v := range b {
+		fmt.Println("i: " + strconv.Itoa(i) + ", v: " + strconv.Itoa(v))
+	}
+
+	for _, v := range b {
+		fmt.Println("v: " + strconv.Itoa(v))
+	}
 }
